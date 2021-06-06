84f8ea7ea34995b0b1270defa84f46807ec112d4
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-06-06 20:13:26 -0300

    arquitetura - carga inicial

---
+++ app/controllers/userController.php
@@ -0,0 +1,26 @@
+<?php
+class userController extends controllerHelper{
+    public function index(){
+        $data = array();
+
+        $this->loadTemplate('user-profile', $data);
+    }
+
+    public function register(){
+        $data = array();
+        $data['css'] = 'user-register.css';
+        $data['js'] = 'user-register.js';
+
+        $this->loadView('user-register', $data);
+    }
+
+    public function login(){
+        $data = array();
+
+        $this->loadView('user-login', $data);
+    }
+
+    public function first_access(){
+        echo "<h1>Primeiro Acesso!</h1>";
+    }
+}
---
+++ app/views/user-register.php
@@ -0,0 +1,34 @@
+<!-- <div id="bg_content">
+    <div id="title_bg">Cadastre-se como candidato</div>
+    <div id="principal_content_area">
+        <div id="col-1">
+            <div id="sub-title_bg">Olá, Enfermeiro(a)!</div>
+            <div id="txt">
+            Estamos aqui para te ajudar. Por isso precisamos de algumas 
+            informações pessoais e profissionais para te auxiliar
+            na sua busca pelo emprego. E evite erros ortográficos e seja sincero, 
+            assim aumentando suas chances de contratação
+            </div>
+        </div>
+
+        <div id="col-2">
+        </div>
+    </div>
+</div> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="<?=BASE_URL?>app/assets/js/jquery.js"></script>
+    <script src="<?=BASE_URL?>app/assets/js/user-register.js"></script>
+    <link rel="stylesheet" href="<?=BASE_URL?>app/assets/css/user-register.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h1>registro de usuário</h1>
+</body>
+</html>
