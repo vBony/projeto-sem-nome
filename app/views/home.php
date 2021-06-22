1fe358e94f2aebaa5a66c54ca044261cce704454
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-06-21 23:25:56 -0300

    esqueleto do carrossel da sessão conheça a quero emprego

--- app/views/home.php
+++ app/views/home.php
@@ -85,5 +85,53 @@
         </div>
 
     </div>
+
+    <div id="about-site">
+        <div class="about-site-inv">
+            <div id="title-about-area">
+                <h4>Conheça a <span>Quero Emprego</span></h4>
+            </div>
+
+            <div id="carroussel-about-area">
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <!-- <img src="..." class="d-block w-100" alt="..."> -->
+                            <div class="carousel-caption d-none d-md-block">
+                                <div>
+                                    <h1>teste</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <!-- <img src="..." class="d-block w-100" alt="..."> -->
+                            <div class="carousel-caption d-none d-md-block">
+                                <h1>teste2</h1>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <!-- <img src="..." class="d-block w-100" alt="..."> -->
+                            <div class="carousel-caption d-none d-md-block">
+                                <h1>teste3</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
