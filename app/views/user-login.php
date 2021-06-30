a7cc47f99dd954c315fdea6b8ce0020308f77784
MATHEUS <matheus8142@hotmail.com>
2021-06-29 22:08:53 -0300

    Tela de login criada com responsividade

--- app/views/user-login.php
+++ app/views/user-login.php
@@ -4,30 +4,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="<?=$_ENV['BASE_URL'] . 'app/assets/js/jquery.js'?>"></script>
+    <link rel="stylesheet" href="<?=$_ENV['BASE_URL'] . 'app/assets/libraries/bootstrap.css'?>">
+    <script src="<?=$_ENV['BASE_URL'] . 'app/assets/libraries/bootstrap.js'?>"></script>
     <link rel="stylesheet" href="<?=$_ENV['BASE_URL'] . 'app/assets/css/user-login.css'?>">
     <link rel="icon" href="<?=$_ENV['BASE_URL']?>app/assets/imgs/logo-mini.png" type="image/png" sizes="16x16">
-    <title>Quero emprego - login</title>
+    <script src="<?=$_ENV['BASE_URL'] . 'app/assets/js/user-login.js'?>"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Quero emprego - Login 🔐</title>
+    
 </head>
 <body>
-<div class="container-login">
-    <div id="login-box">
-        <h1 class="text-center titulo">Login <br> Quero emprego</h1>
+    <div id="app">
+        <div id="box">
+        
+            <div id="title-area">Login como candidato</div>
+            <form id="login-area" method="post">
 
-        <form>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">Jamais compartilhe seu email com ninguém.</div>
+                <div class="input-area">
+                    <div class="label-input">Email</div>
+                    <input required id="email" type="email" name="email">
+                    <div class="invalid-msg" id="email-msg"></div>
+                </div>
+
+                <div class="input-area">
+                    <div class="label-input">Senha</div>
+                    <input required id="senha" type="password" name="senha">
+                    <div class="invalid-msg" id="senha-msg"></div>
+                </div>
+                <div id="esq-area">
+                    <div id="esqueci">Esqueci a senha</div>
+                </div>
+                <div id="buttons-area">
+                    <input type="submit" value="Enviar">
+                    <div class="hr-sect">OU</div>
+                    <div id="sociais-area">
+                        <div class="social-btn">
+                            <div class="sb-icon"><i class="fab fa-google"></i></div>
+                            <div class="sb-text">Google</div>
+                        </div>
+                        <div class="social-btn">
+                            <div class="sb-icon"><i class="fab fa-linkedin"></i></div>
+                            <div class="sb-text">Linkedin</div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </form>
+
+            <div id="redirect-area">
+                <div class="ra empresa">&#8249; Sou empresa</div>
+                <div class="ra registrar"> Registrar-se &#8250;</div>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <button type="submit" class="btn btn-block  btn-success">Enviar</button>
-        </form>
+            
+            
+
+        </div>
+
     </div>
-    
-</div>
-    
 </body>
 </html>
