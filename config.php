1e96d5e8c417d809e2b90769502772699232b35f
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-07-01 22:01:09 -0300

    Relatorio de acessos

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 require 'vendor/autoload.php';
+date_default_timezone_set("America/Sao_Paulo");
 
 use Symfony\Component\Dotenv\Dotenv;
 
@@ -8,7 +9,7 @@ $dotenv->loadEnv(__DIR__.'/.env');
 
 global $db;
 try{
-    // $db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
+    $db = new PDO("mysql:dbname=".$_ENV['DB_NAME'].";host=".$_ENV['HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 }catch(PDOException $e){
     echo "Error: ".$e->getMessage();
     exit;
